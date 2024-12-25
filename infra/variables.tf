a47d0f5ed6811c9c7f40801dbf2cf23114edc5e1
Ron Rindzonski <ron.rindzonski@broadcom.com>
2024-12-25 13:25:05 +0200

    fix user_data in the terraformCD

--- infra/variables.tf
+++ infra/variables.tf
@@ -27,14 +27,14 @@ variable "ec2_map" {
         "GitHub_Runner" = {
             user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y libicu
+              yum update -y
+              yum install -y libicu
               mkdir actions-runner && cd actions-runner
               curl -o actions-runner-linux-x64-2.321.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.321.0/actions-runner-linux-x64-2.321.0.tar.gz
               tar xzf ./actions-runner-linux-x64-2.321.0.tar.gz
               ./config.sh --url https://github.com/rindjon/devops_final_project --token AGT5EOQQXJ6IT75U723J53THNPL6K --unattended
-              sudo ./svc.sh install
-              sudo ./svc.sh start
+              ./svc.sh install
+              ./svc.sh start
             EOF
         }
         "K8S_Cluster" = {}
