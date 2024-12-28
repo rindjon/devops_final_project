82505364abf4390ca6d9359723e4ee8c72350dd8
Ron Rindzonski <ron.rindzonski@broadcom.com>
2024-12-28 16:16:06 +0200

    change infra and configs to get s3 details

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -4,6 +4,33 @@ resource "aws_s3_bucket" "main" {
   tags = {
     Name = var.name
   }
+
+  # Configure public access block settings
+  # block_public_acls = false
+  # block_public_policy = false
+  # ignore_public_acls = false
+  # restrict_public_buckets = false
+
+}
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "PublicReadGetObject",
+#             "Effect": "Allow",
+#             "Principal": "*",
+#             "Action": "s3:GetObject",
+#             "Resource": "arn:aws:s3:::var.bucket_name/*"
+#         }
+#     ]
+# }
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 output "bucket_arn" {
