d57884cb5b20e7c3006ab7e862a4313c7078f2ad
Ron Rindzonski <ron.rindzonski@broadcom.com>
2025-01-01 18:41:01 +0200

    app: add s3 key and id

--- app/app.py
+++ app/app.py
@@ -27,6 +27,8 @@ app = Flask(__name__)
 
 s3 = boto3.client(
     's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name='us-east-1'
 )
 
