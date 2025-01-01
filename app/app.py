7ae014ef19a9c989df6b4dbbe4c60e29e54f80e3
Ron Rindzonski <ron.rindzonski@broadcom.com>
2025-01-01 19:44:59 +0200

    app: try excpet for s3

--- app/app.py
+++ app/app.py
@@ -25,12 +25,18 @@ app = Flask(__name__)
 #     region_name='us-east-1'
 # )
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv('AWS_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET'),
-    region_name='us-east-1'
-)
+try:
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.getenv('AWS_ID'),
+        aws_secret_access_key=os.getenv('AWS_SECRET'),
+        region_name='us-east-1'
+    )
+except Exception as e:
+    s3 = boto3.client(
+        's3',
+        region_name='us-east-1'
+    )
 
 def get_s3_photos(bucket_name):
     try:
