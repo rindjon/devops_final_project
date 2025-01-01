d436925c19829298863d935f877a813fb08aeaa4
Ron Rindzonski <ron.rindzonski@broadcom.com>
2025-01-01 19:11:23 +0200

    app: change the secrets names

--- app/app.py
+++ app/app.py
@@ -27,8 +27,8 @@ app = Flask(__name__)
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    aws_access_key_id=os.getenv('AWS_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET'),
     region_name='us-east-1'
 )
 
